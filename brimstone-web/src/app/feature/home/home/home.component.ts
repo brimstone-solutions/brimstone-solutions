6e62062e47f248115748707d0d064ab650bb96d6
Ivan <ivanlopezokk@gmail.com>
2024-01-28 17:28:32 -0300

    home con una animacion

--- brimstone-web/src/app/feature/home/home/home.component.ts
+++ brimstone-web/src/app/feature/home/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
+import Aos from 'aos';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   constructor(private viewportScroller: ViewportScroller) { }
 
+
+  ngOnInit(): void {
+      Aos.init();
+  }
   scrollToSection(sectionId: string): void {
     this.viewportScroller.scrollToAnchor(sectionId);
   }
--- brimstone-web/src/app/shared/components/box-grande/box-grande.component.ts
+++ brimstone-web/src/app/shared/components/box-grande/box-grande.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { trigger, style,transition,animate, state } from '@angular/animations'
 
 @Component({
   selector: 'app-box-grande',
   templateUrl: './box-grande.component.html',
-  styleUrl: './box-grande.component.scss'
+  styleUrl: './box-grande.component.scss',
+  // animations: [
+  //   trigger('enterSate', [
+  //     state('void', style({
+  //       transform: 'translateX(-100%)',
+  //       opacity: 0
+  //     })),
+  //     transition(':enter',[
+  //       animate(300,style({
+  //         transform:'translateX(0)',
+  //         opacity: 1
+  //       }))
+  //     ])
+  //   ])
+  // ]
 })
 export class BoxGrandeComponent {
 
